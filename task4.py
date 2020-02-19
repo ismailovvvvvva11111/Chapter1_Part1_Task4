7ca78ba8d153304f3f83e1f4bc5d18b7a17c7765
ismailovvvvvva11111 <ismailovvvvaaa24@gmail.com>
2020-02-19 20:36:16 +0600

    Tiji

---
+++ task4.py
@@ -0,0 +1,17 @@
+
+def last_letter(word):
+    words = word[-1]
+    return words
+    
+
+def test_last_letter():
+   
+    assert last_letter('hello!') == '!'
+    assert last_letter('banana') == 'a'
+    assert last_letter('8') == '8'
+    assert last_letter('funnyguys') == 's'
+    assert last_letter('101') == '1'
+    print("YOUR CODE IS CORRECT!")
+
+    #test your code by un-commenting the line(s) below
+test_last_letter()
